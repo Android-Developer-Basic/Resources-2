2fe2f036bed1c2af2241fcd1ae69616b7153b970
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-11-20 12:20:05 +0100

    Typeface from code

--- app/src/main/java/otus/gpb/appresources2/MainActivity.kt
+++ app/src/main/java/otus/gpb/appresources2/MainActivity.kt
@@ -1,11 +1,22 @@
 package otus.gpb.appresources2
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var hello: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hello = findViewById(R.id.hello)
+        setFont()
+    }
+
+    private fun setFont() {
+        hello.setTypeface(resources.getFont(R.font.creato), Typeface.BOLD)
     }
 }
