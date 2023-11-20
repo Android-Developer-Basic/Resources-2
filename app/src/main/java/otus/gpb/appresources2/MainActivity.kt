8f4b3ef2eb5087f61f19d986b7d0d390f030d349
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-11-20 13:12:39 +0100

    Downloadable font

--- app/src/main/java/otus/gpb/appresources2/MainActivity.kt
+++ app/src/main/java/otus/gpb/appresources2/MainActivity.kt
@@ -6,17 +6,8 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var hello: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        hello = findViewById(R.id.hello)
-        setFont()
-    }
-
-    private fun setFont() {
-        hello.setTypeface(resources.getFont(R.font.creato), Typeface.BOLD)
     }
 }
