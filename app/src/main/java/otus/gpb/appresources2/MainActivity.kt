665eef347137a0b5005d539af3f23494e9cf9f41
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-11-21 11:31:21 +0100

    Text appearance from code

--- app/src/main/java/otus/gpb/appresources2/MainActivity.kt
+++ app/src/main/java/otus/gpb/appresources2/MainActivity.kt
@@ -6,8 +6,17 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var hello: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hello = findViewById(R.id.hello)
+        setTextFormat()
+    }
+
+    private fun setTextFormat() {
+        hello.setTextAppearance(R.style.AllCapsLabel)
     }
 }
