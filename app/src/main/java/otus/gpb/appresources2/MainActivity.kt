9c656036a064ee9465e8fd07148a347308a12613
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-11-21 13:13:49 +0100

    Custom attributes

--- app/src/main/java/otus/gpb/appresources2/MainActivity.kt
+++ app/src/main/java/otus/gpb/appresources2/MainActivity.kt
@@ -1,12 +1,9 @@
 package otus.gpb.appresources2
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var hello: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -14,12 +11,5 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.Theme_AppResources2)
 
         setContentView(R.layout.activity_main)
-
-        hello = findViewById(R.id.hello)
-        setTextFormat()
-    }
-
-    private fun setTextFormat() {
-        hello.setTextAppearance(R.style.AllCapsLabel)
     }
 }
