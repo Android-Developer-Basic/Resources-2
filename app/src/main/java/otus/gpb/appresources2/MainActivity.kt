d1197b001b3a021397eae211078efdf661c87df3
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-11-21 13:03:17 +0100

    Applying theme from code

--- app/src/main/java/otus/gpb/appresources2/MainActivity.kt
+++ app/src/main/java/otus/gpb/appresources2/MainActivity.kt
@@ -1,17 +1,18 @@
 package otus.gpb.appresources2
 
-import android.graphics.Typeface
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.core.content.res.getColorOrThrow
-import androidx.core.content.withStyledAttributes
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var hello: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Делайте это до setContentView!
+        setTheme(R.style.Theme_AppResources2)
+
         setContentView(R.layout.activity_main)
 
         hello = findViewById(R.id.hello)
